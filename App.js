30eaf47f9d94fe38d75c3edecf5187faac27af78
DenilsonCunha <denilsonluciano6@gmail.com>
2021-07-22 12:53:55 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,6 +9,12 @@ export default function App() {
         const alt = altura / 100;
         const imc = peso / (alt * alt);
 
-        if(imc)
+        if(imc < 18.6){
+            alert('Você está abaixo do peso! ' + imc.topFixed(2));
+        }else if(imc >= 18.6 && imc < 24.9){
+            alert('Peso ideal! ' + imc.toFixed(2));
+        }else if(imc >= 24.9 && imc < 34.9){
+            alert('Levemente acima do peso! ' + imc.toFixed(2));
+        }
     }
-}
+};
